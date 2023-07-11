40f2a7375a2b946a02222b88bcac310cdac4aa0a
HwaSup <ghktjq1119@naver.com>
2023-07-11 21:27:54 +0900

    feat : Spring Security 환경 세팅을 위해 SpringConfig class 추가

---
+++ src/main/java/com/cmc/zenefitserver/global/config/security/SecurityConfig.java
@@ -0,0 +1,79 @@
+package com.cmc.zenefitserver.global.config.security;
+
+import com.cmc.zenefitserver.global.auth.jwt.JwtAccessDeniedHandler;
+import com.cmc.zenefitserver.global.auth.jwt.JwtAuthenticationEntryPoint;
+import com.cmc.zenefitserver.global.auth.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+
+public class SecurityConfig {
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers(
+                        "/v3/api-docs/**",
+                        "/swagger-resources/**",
+                        "/swagger-ui/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin().disable() // 폼 로그인 비활성화
+                .httpBasic().disable() // HTTP 기본 인증 비활성화
+                .csrf().disable() // CSRF 비활성화
+                .cors().configurationSource(corsConfigurationSource()) // CORS 설정 활성화
+                .and()
+                .exceptionHandling() // 예외 처리 핸들러 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT 를 사용할 것이기 때문에 세션 사용 X
+                .and()
+                //권한이 필요한 요청에 대한 설정
+                .authorizeRequests()
+                .antMatchers("/test/**").permitAll()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+//                .headers().frameOptions().sameOrigin() // 같은 출처의 페이지만 현재 페이지를 프레임으로 사용할 수 있게 허용하는 설정 ( clickjacking 방지 )
+//                .and()
+                .build();
+    }
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
