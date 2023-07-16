9a9622c8d02556071899d240f6d200011546a883
HwaSup <ghktjq1119@naver.com>
2023-07-16 23:38:58 +0900

    feat : oauth 이용해서 kakao 로그인 구현

---
+++ src/main/java/com/cmc/zenefitserver/global/auth/kakao/KakaoAccount.java
@@ -0,0 +1,11 @@
+package com.cmc.zenefitserver.global.auth.kakao;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class KakaoAccount {
+    private String gender;
+    private String email;
+}
