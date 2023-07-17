7ccaab999c7e8d99677676bae4489fa536138679
HwaSup <ghktjq1119@naver.com>
2023-07-17 22:41:46 +0900

    docs : Swagger2 -> Swagger3 로 변경

--- src/main/java/com/cmc/zenefitserver/global/common/request/AuthRequestDto.java
+++ src/main/java/com/cmc/zenefitserver/global/common/request/AuthRequestDto.java
@@ -3,6 +3,7 @@ package com.cmc.zenefitserver.global.common.request;
 import com.cmc.zenefitserver.global.auth.ProviderType;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,14 +17,14 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @NoArgsConstructor
 @Validated
-@ApiModel(value = "로그인 API Request")
+@Schema(description = "로그인 API Request")
 public class AuthRequestDto {
 
-    @ApiModelProperty(notes = "소셜 로그인 유형", example = "APPLE, KAKAO, GOOGLE, NAVER")
+    @Schema(description = "소셜 로그인 유형",example = "KAKAO")
     @NotNull
     private ProviderType providerType;
 
-    @ApiModelProperty(notes = "소셜 토큰 (kakao는 code)", example = "p1b3M_ikmtHivvFJqwY5bXAYg-ilCq4E7DnJwlT5CisM0wAAAYOPy1oR")
+    @Schema(description = "소셜 토큰 (kakao는 code)", example = "p1b3M_ikmtHivvFJqwY5bXAYg-ilCq4E7DnJwlT5CisM0wAAAYOPy1oR")
     @NotNull
     private String token;
 }
