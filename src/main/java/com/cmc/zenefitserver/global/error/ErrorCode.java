daa9b7ab4a9786548f61e19a8b61883c4c5b2f0c
HwaSup <ghktjq1119@naver.com>
2023-08-12 16:07:10 +0900

    refactor : ErrorCode 클래스에서 Common 관련 오류 이름 변경 및 추가

--- src/main/java/com/cmc/zenefitserver/global/error/ErrorCode.java
+++ src/main/java/com/cmc/zenefitserver/global/error/ErrorCode.java
@@ -42,12 +42,15 @@ public enum ErrorCode {
     /**
      * Common 관련 오류
      */
-    NOT_FOUND_GENDER_ENUM_VALUE("COMMON_001", "GENDER 가 올바르지 않은 값입니다.", UNAUTHORIZED),
-    NOT_FOUND_EDUCATION_ENUM_VALUE("COMMON_002", "EDUCATION 이 올바르지 않은 값입니다.", UNAUTHORIZED),
-    NOT_FOUND_JOB_ENUM_VALUE("COMMON_003", "JOB 이 올바르지 않은 값입니다.", UNAUTHORIZED),
-    NOT_FOUND_SPLZ_ENUM_VALUE("COMMON_003", "splzRlmRqisCn 이 올바르지 않은 값입니다.", UNAUTHORIZED),
-    NOT_FOUND_CITY_ENUM_VALUE("COMMON_004", "area 값이 올바르지 않은 값입니다.", UNAUTHORIZED),
-    NOT_FOUND_PROVIDER_ENUM_VALUE("COMMON_005", "provider 값이 올바르지 않은 값입니다.", UNAUTHORIZED);
+    NOT_FOUND_GENDER_ENUM_VALUE("COMMON_001", "gender 값이 올바르지 않은 값입니다.", UNAUTHORIZED),
+    NOT_FOUND_EDUCATION_ENUM_VALUE("COMMON_002", "education 값이 올바르지 않은 값입니다.", UNAUTHORIZED),
+    NOT_FOUND_JOB_ENUM_VALUE("COMMON_003", "job 이 올바르지 않은 값입니다.", UNAUTHORIZED),
+    NOT_FOUND_SPLZ_ENUM_VALUE("COMMON_004", "splzRlmRqisCn 이 올바르지 않은 값입니다.", UNAUTHORIZED),
+    NOT_FOUND_AREA_ENUM_VALUE("COMMON_005", "area 값이 올바르지 않은 값입니다.", UNAUTHORIZED),
+    NOT_FOUND_PROVIDER_TYPE_ENUM_VALUE("COMMON_006", "providerType 값이 올바르지 않은 값입니다.", UNAUTHORIZED),
+    NOT_FOUND_SUPPORT_POLICY_TYPE_ENUM_VALUE("COMMON_007", "supportPolicyType 값이 올바르지 않은 값입니다.", UNAUTHORIZED),
+    NOT_FOUND_POLICY_TYPE_ENUM_VALUE("COMMON_008", "policyType 값이 올바르지 않은 값입니다.", UNAUTHORIZED);;
+
 
 
     private final String code;
