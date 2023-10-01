7cc37b61da692fe44e90b6e2d00191622fd2aa00
LHS-11 <ghktjq1119@naver.com>
2023-10-01 17:32:01 +0900

    refactor : 회원가입시 닉네임 컬럼 제거

--- src/main/java/com/cmc/zenefitserver/domain/user/dto/SignUpRequestDto.java
+++ src/main/java/com/cmc/zenefitserver/domain/user/dto/SignUpRequestDto.java
@@ -2,7 +2,6 @@ package com.cmc.zenefitserver.domain.user.dto;
 
 import com.cmc.zenefitserver.domain.policy.domain.enums.AreaCode;
 import com.cmc.zenefitserver.domain.policy.domain.enums.CityCode;
-import com.cmc.zenefitserver.domain.user.domain.Address;
 import com.cmc.zenefitserver.domain.user.domain.EducationType;
 import com.cmc.zenefitserver.domain.user.domain.Gender;
 import com.cmc.zenefitserver.domain.user.domain.JobType;
@@ -13,7 +12,6 @@ import lombok.*;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import java.util.Set;
 
 
@@ -24,47 +22,47 @@ import java.util.Set;
 public class SignUpRequestDto {
 
     @NotNull(message = "이메일을 입력해주세요.")
-    @ApiModelProperty(notes = "이메일",example = "awdsawd@naver.com")
+    @ApiModelProperty(notes = "이메일", example = "awdsawd@naver.com")
     private String email;
 
     @NotNull(message = "소셜 타입을 입력해주세요.")
-    @ApiModelProperty(notes = "소셜 타입",example = "KAKAO")
+    @ApiModelProperty(notes = "소셜 타입", example = "KAKAO")
     private ProviderType provider;
 
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣]{1,7}$", message = "닉네임은 1~7자의 영문 대소문자, 숫자, 한글로만 입력해주세요.")
-    @ApiModelProperty(notes ="닉네임",example = "paul")
-    private String nickname;
+//    @Pattern(regexp = "^[a-zA-Z0-9가-힣]{1,7}$", message = "닉네임은 1~7자의 영문 대소문자, 숫자, 한글로만 입력해주세요.")
+//    @ApiModelProperty(notes ="닉네임",example = "paul")
+//    private String nickname;
 
     @NotNull(message = "나이를 입력해주세요.")
-    @ApiModelProperty(notes = "나이",example = "25")
+    @ApiModelProperty(notes = "나이", example = "25")
     private int age;
 
     @NotNull(message = "areaCode 를 입력해주세요.")
-    @ApiModelProperty(notes = "시/도",example = "서울")
+    @ApiModelProperty(notes = "시/도", example = "서울")
     private AreaCode areaCode;
 
     @NotNull(message = "cityCode 를 입력해주세요.")
-    @ApiModelProperty(notes = "시/구",example = "강서구")
+    @ApiModelProperty(notes = "시/구", example = "강서구")
     private CityCode cityCode;
 
     @NotNull(message = "작년 소득을 입력해주세요.")
-    @ApiModelProperty(notes = "작년 소득",example = "50000000")
+    @ApiModelProperty(notes = "작년 소득", example = "50000000")
     private int lastYearIncome;
 
-    @ApiModelProperty(notes = "학력",example = "대학 재학")
+    @ApiModelProperty(notes = "학력", example = "대학 재학")
     private EducationType educationType;
 
-    @ApiModelProperty(notes = "직업",example = "재직자")
+    @ApiModelProperty(notes = "직업", example = "재직자")
     private Set<JobType> jobs;
 
-    @ApiModelProperty(notes = "성별",example = "MALE")
+    @ApiModelProperty(notes = "성별", example = "MALE")
     private Gender gender;
 
     @Builder
-    public SignUpRequestDto(String email, ProviderType provider, String nickname, int age, AreaCode areaCode, CityCode cityCode, int lastYearIncome, EducationType educationType, Set<JobType> jobs, Gender gender) {
+    public SignUpRequestDto(String email, ProviderType provider, int age, AreaCode areaCode, CityCode cityCode, int lastYearIncome, EducationType educationType, Set<JobType> jobs, Gender gender) {
         this.email = email;
         this.provider = provider;
-        this.nickname = nickname;
+//        this.nickname = nickname;
         this.age = age;
         this.areaCode = areaCode;
         this.cityCode = cityCode;
