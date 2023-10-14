db631be08448b69788d4190ea2bc0c22f1984364
LHS-11 <ghktjq1119@naver.com>
2023-10-14 22:15:17 +0900

    fix : ErrorCode 에서 code String -> int 로 바꿈에 exception 도 맞춰서 수정

--- src/main/java/com/cmc/zenefitserver/global/auth/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/cmc/zenefitserver/global/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -1,16 +1,19 @@
 package com.cmc.zenefitserver.global.auth.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import com.cmc.zenefitserver.global.error.ErrorCode;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @Component
 @RequestMapping
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -18,37 +21,45 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        String exception = (String) request.getAttribute("exception");
 
+        Integer exception = (Integer) request.getAttribute("exception");
+
+        log.debug("exception" + exception);
         // 토큰이 없을 경우
-        if(exception==null || exception.equals(ErrorCode.NOT_FOUND_TOKEN.getCode())){
-            setResponse(response,ErrorCode.NOT_FOUND_TOKEN);
+        if (exception == null || exception.equals(ErrorCode.NOT_FOUND_TOKEN.getCode())) {
+            setResponse(response, ErrorCode.NOT_FOUND_TOKEN);
             return;
         }
 
         // 잘못된 타입의 토큰일 경우
-        if(exception.equals(ErrorCode.INVALID_AUTH_TOKEN.getCode())){
-            setResponse(response,ErrorCode.INVALID_AUTH_TOKEN);
+        if (exception.equals(ErrorCode.INVALID_AUTH_TOKEN.getCode())) {
+            setResponse(response, ErrorCode.INVALID_AUTH_TOKEN);
             return;
         }
 
         // 잘못된 서명
-        if(exception.equals(ErrorCode.INVALID_SIGNATURE_TOKEN.getCode())){
-            setResponse(response,ErrorCode.INVALID_SIGNATURE_TOKEN);
+        if (exception.equals(ErrorCode.INVALID_SIGNATURE_TOKEN.getCode())) {
+            setResponse(response, ErrorCode.INVALID_SIGNATURE_TOKEN);
             return;
         }
 
         // 토큰 만료된 경우
-        if(exception.equals(ErrorCode.EXPIRED_TOKEN.getCode())){
-            setResponse(response,ErrorCode.EXPIRED_TOKEN);
+        if (exception.equals(ErrorCode.EXPIRED_TOKEN.getCode())) {
+            setResponse(response, ErrorCode.EXPIRED_TOKEN);
             return;
         }
 
         // 지원되지 않는 토큰인 경우
-        if(exception.equals(ErrorCode.UNSUPPORTED_TOKEN.getCode())){
-            setResponse(response,ErrorCode.UNSUPPORTED_TOKEN);
+        if (exception.equals(ErrorCode.UNSUPPORTED_TOKEN.getCode())) {
+            setResponse(response, ErrorCode.UNSUPPORTED_TOKEN);
             return;
         }
+
+        // 토큰에 해당하는 유저가 데이터베이스에 없을 경우
+        if (exception.equals(ErrorCode.NOT_FOUND_USER.getCode())) {
+            setResponse(response, ErrorCode.NOT_FOUND_USER);
+        }
+
 
     }
 
