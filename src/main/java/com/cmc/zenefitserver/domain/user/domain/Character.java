05b92a1035274e0ff909d27264c3b0725c775712
LHS-11 <ghktjq1119@naver.com>
2023-11-08 23:34:21 +0900

    style : 캐릭터 문구 마지막 온점 제거하도록 수정

--- src/main/java/com/cmc/zenefitserver/domain/user/domain/Character.java
+++ src/main/java/com/cmc/zenefitserver/domain/user/domain/Character.java
@@ -11,7 +11,7 @@ public enum Character {
 
     NEW("new", "아직은 상위 60%지만 더 노력해봐요!", sumPolicyCount -> sumPolicyCount >= 4 && sumPolicyCount <= 9),
     SAVE("save", "또래 친구들 중 상위 30%로 똑부러지는 편이네요", sumPolicyCount -> sumPolicyCount >= 10 && sumPolicyCount <= 12),
-    SMART("smart", "또래 친구들 중 상위 5%로 앞서 가고 있어요.", sumPolicyCount -> sumPolicyCount >= 13);
+    SMART("smart", "또래 친구들 중 상위 5%로 앞서 가고 있어요", sumPolicyCount -> sumPolicyCount >= 13);
 
 
     private final String name;
