30aa707879b38edb65879e2dea6546b8334f72b9
LHS-11 <ghktjq1119@naver.com>
2024-02-18 21:19:51 +0900

    fix : 알림 스케줄러 주기 오전 00:01 마다 실행하도록 구현

--- src/main/java/com/cmc/zenefitserver/global/infra/notification/application/NotificationScheduler.java
+++ src/main/java/com/cmc/zenefitserver/global/infra/notification/application/NotificationScheduler.java
@@ -32,8 +32,7 @@ public class NotificationScheduler {
     private final NotificationRepository notificationRepository;
 
 
-//    @Scheduled(cron = "0 0 0 * * *") // 매일 오전 12시에 실행
-    @Scheduled(cron = "0 0/3 * * * *")// 10분 주기로 실행
+    @Scheduled(cron = "0 1 0 1/1 * ?")// 10분 주기로 실행
     public void notifyUser() {
         System.out.println("LocalDate.now() = " + LocalDate.now());
         notifyUserBySttDate(LocalDate.now()); // 신청 시작일 기준
